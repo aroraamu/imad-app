5bda1927bf6f5cecf5a7d69bfbc15a15509360de
aroraamu <aroraamu111@gmail.com>
2017-10-25 01:56:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -195,11 +195,27 @@ var names = [];
 	res.send(JSON.stringify(names)); // <= convert array into string
 });
 
-app.get('/:articleName', function (req, res) {
-	// articlename = articlethree
-	// articles[articlename] == {} content object for article-three
-	var articleName = req.params.articleName;
-	res.send(createTemplate(articles[articleName]));
+//app.get('/:articleName', function (req, res) {
+//	// articlename = articlethree
+//	// articles[articlename] == {} content object for article-three
+//	var articleName = req.params.articleName;
+//	res.send(createTemplate(articles[articleName]));
+//});
+
+// replace above code by code to fetch form db
+app.get('/articles/:articleName', function (req, res) {
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err,result) {
+        if (err) {
+            res.status(500).send(err.toString);
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = results.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
