8fdef112144c6714fee99c753a9df45efafdb1d4
aroraamu <aroraamu111@gmail.com>
2017-10-24 18:32:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.get('/test-db', function (req, res) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            res.send(JSON.stringify(result));
+            //res.send(JSON.stringify(result)); //returns entire JSON object
+            res.send(JSON.stringify(result.rows)); // returns just the rows
         }
     });
 });
