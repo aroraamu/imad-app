706486e5613b8d948b4433cc0915414414e8e7bb
aroraamu <aroraamu111@gmail.com>
2017-11-07 19:01:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD //environment variable => db-aroraamu111-24009
 };
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -26,6 +27,19 @@ var port = prod;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt) {  
+    //how do we crea    te a hash
+    //const crypto = require('rypto');
+    //const key = crypto.pbkdf2Sync('secret', 'salt', 100000, 64, 'sha512');
+    //console.log(key.toString('hex'));  // '3745e48...08d59ae'
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-a-random-string');
+   res.send(hashedString);
 });
 
 var pool = new Pool(config);
